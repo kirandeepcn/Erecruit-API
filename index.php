7a964adab087d0c350851561913787d0e5b3ebb0
Kirandeep Singh <kirandeep@codenomad.net>
2015-04-18 23:58:37 +0530

    Minor changes

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @author - Kirandeep Singh
+ * @organization - Codenomad
  */
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
